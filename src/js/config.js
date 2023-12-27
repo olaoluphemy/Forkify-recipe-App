5dfce8de703b03e13945492da111f6308e578601
olaoluphemy <bidesim8@gmail.com>
2023-12-27 12:08:56 +0100

    Second Commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = "a82850c0-82ec-4342-ad89-63dcd01b4d36";
+export const KEY = "0bac3b0f-784e-40c6-a4d8-671c3baa2a1e";
