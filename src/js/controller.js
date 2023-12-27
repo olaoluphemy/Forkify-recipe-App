561bbe5527580eff145e6900abf1ae2570dfc432
olaoluphemy <bidesim8@gmail.com>
2023-12-27 19:24:49 +0100

    latest commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -21,7 +21,8 @@ const controlRecipes = async function () {
 
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
-    resultsView.update(model.state.search.recipeResults);
+    resultsView.update(model.state.search.recipes);
+    console.log(model.state.search.recipes);
 
     // 3) Render bookmarks
     if (model.state.bookmarks.length === 0) return;
